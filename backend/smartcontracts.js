60d87e1065a36add15cc2b667c7d685f3c320c62
Kent <khmakishima@ucdavis.edu>
2018-04-28 12:37:29 -0700

    skeleton contracts

---
+++ backend/smartcontracts.js
@@ -0,0 +1,29 @@
+'use strict';
+
+
+var taxIRS = function (text) {
+	if(text){
+
+	}
+	else{
+
+	}
+};
+
+
+var veridium = function(){
+
+};
+
+var transaction2charito = function(text){
+	if(text){
+
+	}
+	else{
+
+	}
+};
+
+var charito = function(){
+
+};
